73d554b68cd2246ea0b7217a8a204f2006f90679
stefc400 <83723460+stefc400@users.noreply.github.com>
2021-09-27 18:56:30 -0400

    Create react app + added Bootstrap + SheCodes logo

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import logo from "./logo.png";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo img-fluid" alt="logo" />
+      </div>
+    </div>
+  );
+}
