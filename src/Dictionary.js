f587964789cfb62f3553c3f7c76ebabc745c6b9d
stefc400 <83723460+stefc400@users.noreply.github.com>
2021-09-28 12:48:55 -0400

    api integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,10 +1,16 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 export default function Dictionary() {
   const [keyword, setKeyword] = useState("");
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
   function search(event) {
     event.preventDefault();
-    alert(`Searching results for ${keyword}`);
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
