5d9930db115c62d79157e6fefedf104137b44e9b
stefc400 <83723460+stefc400@users.noreply.github.com>
2021-10-19 22:44:36 -0400

    Added default Keyword

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default function App() {
           <img src={logo} className="App-logo img-fluid" alt="logo" />
         </div>
         <main>
-          <Dictionary />
+          <Dictionary defaultKeyword="sunset" />
         </main>
       </div>
     </div>
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,39 +2,51 @@ import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
 import "./Dictionary.css";
-export default function Dictionary() {
-  const [keyword, setKeyword] = useState("");
+export default function Dictionary(props) {
+  const [keyword, setKeyword] = useState(props.defaultKeyword);
   const [results, setResults] = useState(null);
+  const [loaded, setLoaded] = useState(false);
   function handleResponse(response) {
     setResults(response.data[0]);
   }
-  function search(event) {
-    event.preventDefault();
+  function search() {
     // documentation: https://dictionaryapi.dev
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
   }
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
-
-  return (
-    <div className="Dictionary">
-      <section>
-        <form onSubmit={search}>
-          <input
-            type="search"
-            autoFocus={true}
-            placeholder="Enter a word..."
-            onChange={handleKeywordChange}
-          ></input>
-        </form>
-        <div className="hint">
-          Suggested words: sunset, wine, yoga, forest, plant...
-        </div>
-      </section>
-      <Results results={results} />
-    </div>
-  );
+  function load() {
+    setLoaded(true);
+    search();
+  }
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <section>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="search"
+              autoFocus={true}
+              placeholder="Enter a word..."
+              onChange={handleKeywordChange}
+            ></input>
+          </form>
+          <div className="hint">
+            Suggested words: sunset, wine, yoga, forest, plant...
+          </div>
+        </section>
+        <Results results={results} />
+      </div>
+    );
+  } else {
+    load();
+    return "Loading";
+  }
 }
